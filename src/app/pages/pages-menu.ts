1edecee590de9e0c0dbd25aea9404776549f1cee
Paweł Taborowski <paweltab@wp.pl>
2018-06-12 02:06:45 +0200

    Usunięcie Dashboardu i bezużytecznych bądź szkodliwych przycisków. Czyszczenie dość powierzchowne: elementów nie ma na stronie, ale w kodzie są.

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -2,12 +2,6 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'Dashboard',
-    icon: 'nb-home',
-    link: '/pages/dashboard',
-    home: true,
-  },
-  {
     title: 'Virtual Machines',
     icon: 'nb-list',
     link: '/pages/virtual-machines',
